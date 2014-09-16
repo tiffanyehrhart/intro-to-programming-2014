17825f02646bae7ce84dd99ec85c23c4d111445f
tiffanyehrhart <tiffanyehrhart@Tiffanys-MacBook-Pro.local>
2014-09-16 16:52:34 -0600

    Exercise 3 Update

--- www/wp-content/themes/intro-to-programming/exercise-3-palindromes.php
+++ www/wp-content/themes/intro-to-programming/exercise-3-palindromes.php
@@ -1,6 +1,15 @@
-<table>
-  <tr>
-    <td>new jersey</td>
-    <td>abba</td>
-  </tr>
-</table>
+<?php
+function Is_Palindrome ($string){
+if ($string == strrev($string)){
+  return true ;
+}
+else {return false ;}
+}
+
+$new_jersey_is_palindrome=Is_Palindrome("new jersey") ;
+if($new_jersey_is_palindrome){
+  echo "Is Palindrome!";
+}
+else{
+  echo "Isn't Palindrome!";}
+?>
